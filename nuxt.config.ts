89a6265db54f26aa1f069377c70f8275fe4853c0
jesskruse <100980906+jesskruse@users.noreply.github.com>
2024-09-11 15:18:54 -0700

    Add code highlighting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxt/content'],
+  content: {
+    highlight: {
+        theme: 'github-light'
+    },
+  },
   compatibilityDate: '2024-09-11'
 })
